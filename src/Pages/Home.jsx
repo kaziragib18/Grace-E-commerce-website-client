7cfba7cda7520230194e18902c4a82f26a607e50
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-11-22 22:12:34 +0600

    Slider added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import Announcement from '../components/Announcement'
-import Navbar from '../components/Navbar'
+import React from 'react';
+import Announcement from '../components/Announcement';
+import Navbar from '../components/Navbar';
+import Slider from '../components/Slider';
 
 const Home = () => {
       return (
             <div>
                   <Announcement></Announcement>
                   <Navbar></Navbar>
+                  <Slider></Slider>
             </div>
       )
 }
 
-export default Home
+export default Home;
--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -5,7 +5,12 @@ const Container = styled.div`
       height: 30px;
       background-color: teal;
       color: white;
-`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-style: 14px;
+      font-weight: 500;
+`;
 
 const Announcement = () => {
       return (
