337a9daa3bd086700343912f9997cd73018f7c69
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-11-24 13:28:33 +0600

    Register & Login page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Home from "./Pages/Home";
 import ProductList from "./Pages/ProductList";
 import Product from "./Pages/Product";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
 
 const App = () => {
-  return <Product />
+  return <Login></Login>;
 };
 
 export default App;
