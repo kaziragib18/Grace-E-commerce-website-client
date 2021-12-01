5a4ab9da87ad8a0f2e27291e46d2136e692c5764
Kazi Md Ragib <58323531+kaziragib18@users.noreply.github.com>
2021-12-01 17:51:22 +0600

    login in authentication done

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,10 @@ import Register from "./Pages/Register";
 import Cart from "./Pages/Cart";
 import Success from "./Pages/Success";
 import { Redirect } from "react-router";
+import { useSelector } from "react-redux";
 
 const App = () => {
-  const user = true
+  const user = useSelector(state => state.user.currentUser);
   return (
     <Router>
       <Switch>
@@ -26,11 +27,9 @@ const App = () => {
         </Route>
         <Route path="/login">
           {user ? <Redirect to="/" /> : <Login></Login>}
-          <Login></Login>
         </Route>
         <Route path="/register">
           {user ? <Redirect to="/" /> : <Register></Register>}
-          <Register></Register>
         </Route>
         <Route path="/products/:category">
           <ProductList></ProductList>
--- src/Pages/Success.jsx
+++ src/Pages/Success.jsx
@@ -5,8 +5,17 @@ const Success = () => {
       const location = useLocation()
       console.log(location)
       return (
-            <div>
-                  SuccessFull
+            <div
+                  style={{
+                        height: "100vh",
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                  }}
+            >
+                  <h1>Payment Successful</h1>
+                  <button style={{ padding: 10, marginTop: 20 }}>Go to Homepage</button>
             </div>
       )
 }
